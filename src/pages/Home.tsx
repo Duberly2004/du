9dd9d169064f4b07fe7e36f72bb3da9255350430
duberly2004 <ivanmondragonmanchay@gmail.com>
2024-08-26 22:42:10 -0500

    ✨ Add: new changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,96 +1,32 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import pedro from "../assets/pedro-pedro.gif"
-import { User, users } from "./data";
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+import gato from '../assets/gato.webp'
 export default function Home() {
-  const [search,setSearch] = useState<string>("")
-  const [loading,setLoading] = useState<boolean>(false)
-  const [noResults,setNoResults] = useState<boolean>(false)
-  const [user,setUser] = useState<User>()
-  const onSearch = () => {
-    setLoading(true)
-    setTimeout(()=>{
-      const user = users.find(user=>user.emotion.toLowerCase()===search)
-      if(user){
-        setUser(user)
-        setNoResults(false)
-      }else{
-        setNoResults(true)
-      }
-      setLoading(false)
-    },4000)
-    console.log(search)
-  }
-
+  const [state,setState] = useState<Boolean>(false) 
+  const [success,setSuccess] = useState<Boolean>(false)
   return (
-<div className="text-center">
-  <div className="max-w-md m-auto mt-10 p-6 bg-white rounded-lg shadow-lg">
-    <h1 className="text-3xl font-extrabold text-gray-800 mb-6">
-      Encontrar personas por palabras clave
-    </h1>
-    <p className="text-left text-gray-600 mb-4">
-      Bienvenid@ a nuestra plataforma. Escribe una palabra clave para buscar a esa persona.
-    </p>
-    <div className="flex items-center space-x-4 mb-6">
-      <Input
-        onChange={(e) => {
-          setNoResults(false);
-          setSearch(e.target.value.toLowerCase());
-        }}
-        placeholder="Escribe aquí"
-        type="search"
-        className="w-full px-4 py-2 border rounded-md focus:ring focus:outline-none"
-      />
-      <Button
-        disabled={search.length === 0}
-        onClick={onSearch}
-        className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 disabled:opacity-50"
-      >
-        Buscar
-      </Button>
-    </div>
-    {loading && <Loading />}
-    {user && (
-      <div className="mt-6 text-left">
-        <h2 className="text-xl font-bold text-gray-700 mb-2">
-          Se encontraron coincidencias con la búsqueda
-        </h2>
-        <p className="text-lg font-medium text-gray-700 mb-1">
-          Resultados para: <span className="text-blue-600">{user.emotion}</span>
-        </p>
-        <p className="text-lg text-gray-600 mb-4">Nombre: <span className="text-blue-600">{user.name}</span></p>
-        <img
-          src={user.photo}
-          alt="user"
-          className="w-44 h-44 rounded-3xl border-2 border-blue-500 object-cover mx-auto mb-4"
-        />
-        <p className="text-md font-semibold text-gray-700 mb-2">
-          Razones por las que es {user.emotion}:
-        </p>
-        <ul className="list-disc list-inside text-left text-gray-600 space-y-1">
-          {user.skills.map((skill) => (
-            <li key={skill}>{skill}</li>
-          ))}
-        </ul>
+    <section>
+        <h1 className="text-center text-2xl mt-6">¿Eres mongola?</h1>
+      <div className="flex justify-center mt-4 space-x-8">
+        {state ?(
+          <>
+          <Button variant="destructive" onClick={()=>setState(!state)}>No</Button>
+          <Button className="bg-blue-500 hover:bg-blue-400" onClick={()=>setSuccess(true)}>Yes</Button>
+          </>
+        ):
+        (
+          <>
+        <Button className="bg-blue-500 hover:bg-blue-400" onClick={()=>setSuccess(true)}>Yes</Button>
+        <Button variant="destructive" onClick={()=>setState(!state)}>No</Button>
+          </>
+      )}
       </div>
-    )}
-    {noResults && (
-      <p className="py-4 text-red-500">
-        No se encontraron resultados para: <b>{search}</b>
-      </p>
-    )}
-  </div>
-</div>
-
+      {success&&(
+        <>
+        <p className="mt-5 text-center">Sabía que dirías que si mongola JAJAJ</p>
+        <img className="mt-7 w-[200px] m-auto" src={gato} alt="gato" />
+        </>
+      )}
+    </section>
   )
-}
-
-function Loading() {
-  return(
-    <div className="flex justify-center flex-col items-center my-4">
-    <img width={200} src={pedro} alt="pedro pedro" />
-    <p>Buscando...</p>
-  </div>
-  ) 
 }
